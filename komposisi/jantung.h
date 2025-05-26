d8f8faed49f6e7e558653ad67a9e297c36e0630f
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-26 16:24:16 +0700

    menambahkan jantung.h

---
+++ komposisi/jantung.h
@@ -0,0 +1,13 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+class jantung {
+public:
+    jantung() {
+        cout << "Jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "Jantung dimatikan\n";
+    }
+};
+#endif
